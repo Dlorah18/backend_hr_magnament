44178df797f2f767dbebfc532a238122d2775a57
unknown <Harold@DESKTOP-DF9MMK4.com>
2023-05-11 19:01:38 -0500

    Puerto base de datos

--- build/config/databases.js
+++ build/config/databases.js
@@ -8,14 +8,15 @@ const mysql_1 = __importDefault(require("mysql"));
 require("dotenv/config");
 class Database {
     constructor() {
-        this.host = String(process.env.HOST_DB || 'localhost');
+        this.host = String(process.env.HOST_DB || 'containers-us-west-40.railway.app');
         this.user = String(process.env.USER_DB || 'root');
-        this.password = String(process.env.PASS_DB || '');
-        this.database = String(process.env.DATABASE_DB || 'hr_magnament');
+        this.password = String(process.env.PASS_DB || 'du8whWC5HIvP9bGGQUiW');
+        this.database = String(process.env.DATABASE_DB || 'railway');
         this.connection = mysql_1.default.createConnection({
             host: this.host,
             user: this.user,
             password: this.password,
+            port     : 6543,
             database: this.database
         });
     }
